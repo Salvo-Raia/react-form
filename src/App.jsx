a7a4fc7879b4ed01bab5a55ec9f5003f43995346
Salvo-Raia <salvo.raia@outlook.it>
2026-02-10 21:37:31 +0100

    Add input DOM element

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './css/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const articleList = ["Transumanesimo 2026", "Roadmap to Mars", "Robot and Humans boundaries"]; 
 
   return (
-    <>
-
-    </>
+    <div className='container py-3'>
+      <div>
+      <h1>Latest articles</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fugit itaque consequatur perspiciatis sapiente asperiores culpa hic. Pariatur, nemo. Ipsum magnam alias at sunt sed blanditiis esse accusantium rerum in?</p>
+      </div>    
+      <div className='list-group'>
+        <ul>
+          {articleList.map((todo) => <li>{todo}</li>)}
+        </ul>
+        <hr />
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+            <div class="input-group-append">
+           <button class="btn btn-outline-secondary" type="button">Button</button>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default App
+
